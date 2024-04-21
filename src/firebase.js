a49eb7976b122b91e18d13fd4d28e8440ae2fa27
Maheen Ilyas <mahilyas05@gmail.com>
2024-04-22 02:59:34 +0530

    Integrated with Firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAj60eYDLpnOZUIHYumaPd01fdOiLjDdN8",
+    authDomain: "clone-d22c9.firebaseapp.com",
+    projectId: "clone-d22c9",
+    storageBucket: "clone-d22c9.appspot.com",
+    messagingSenderId: "50880464565",
+    appId: "1:50880464565:web:7235a2da36e6bd7e88e0fc"
+  };
